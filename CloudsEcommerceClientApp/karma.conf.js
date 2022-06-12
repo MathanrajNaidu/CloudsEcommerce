3b39f88360ffd00c93567c83482935acd0b770fe
mathanraj naidu <mathanrajnaidu@gmail.com>
2022-06-12 23:59:12 +0800

    Added angular app

--- CloudsEcommerceClientApp/karma.conf.js
+++ CloudsEcommerceClientApp/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/clouds-ecommerce'),
+      dir: require('path').join(__dirname, './coverage/clouds-ecommerce-client-app'),
       subdir: '.',
       reporters: [
         { type: 'html' },
